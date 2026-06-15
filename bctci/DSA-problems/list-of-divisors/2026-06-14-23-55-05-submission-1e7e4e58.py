a4654903b4ed2c270ab7959424605d5df422f4c9
Nathaniel Mosco <natemosco@gmail.com>
2026-06-14 23:20:38 -0400

    sync list-of-divisors submission 2026-06-14T23:55:05.131Z

---
+++ bctci/DSA-problems/list-of-divisors/2026-06-14-23-55-05-submission-1e7e4e58.py
@@ -0,0 +1,13 @@
+# Managed by BeyondCTCI one-way sync (force-pushed). Manual edits are not reconciled and may be overwritten by future syncs.
+# Wrong Answer: Submission produced output that did not match the expected output.
+def list_of_divisors(n):
+  divisors = []
+  i = 1
+  while i * i <= n:
+    if n % i == 0:
+      divisors.append(i)
+      if i * i != n:
+        divisors.append(n // i)
+    i += 1
+  divisors.sort()
+  return [1,2,3,4]
